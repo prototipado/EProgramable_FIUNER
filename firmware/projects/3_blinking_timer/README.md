3faa165e09112959a51a47e25d92346efa598290
albanopenalva <apenalva@ingenieria.uner.edu.ar>
2024-03-27 15:03:09 -0300

    Se sube ejemplo 3_blinking_timer, que utiliza tareas y las interrupciones del timer para controlar el blinkeo del LED_1 y LED_2.

---
+++ firmware/projects/3_blinking_timer/README.md
@@ -0,0 +1,11 @@
+# Ejemplo blinking tasks
+
+Este ejemplo hace parpadear el LED 1 (verde 🟢) y el LED 2 (amarillo 🟡) de la placa ESP-EDU, cada uno a una frecuencias fija diferente, utilizando interrupciones del timer para la temporización. 
+
+## Cómo usar el ejemplo
+
+Seguir los pasos detallados en los siguientes instructivos (según sea necesario):
+
+1. [Instalación](../../../documentación/instalación.md)
+2. [Compilación](../../../documentación/compilación.md)
+3. [Grabación y Depuración](../../../documentación/depuración.md)
---
+++ firmware/projects/3_blinking_timer/main/3_blinking_timer.c
@@ -0,0 +1,92 @@
+/*! @mainpage Blinking
+ *
+ * \section genDesc General Description
+ *
+ * This example makes LED_1 and LED_2 blink at different rates, using FreeRTOS tasks and timer interrupts.
+ * 
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 12/09/2023 | Document creation		                         |
+ *
+ * @author Albano Peñalva (albano.penalva@uner.edu.ar)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "timer_mcu.h"
+#include "led.h"
+/*==================[macros and definitions]=================================*/
+#define CONFIG_BLINK_PERIOD_LED_1_US 1000000
+#define CONFIG_BLINK_PERIOD_LED_2_US 1300000
+/*==================[internal data definition]===============================*/
+TaskHandle_t led1_task_handle = NULL;
+TaskHandle_t led2_task_handle = NULL;
+/*==================[internal functions declaration]=========================*/
+/**
+ * @brief Función invocada en la interrupción del timer A
+ */
+void FuncTimerA(void* param){
+    xTaskNotifyGive(led1_task_handle);    /* Envía una notificación a la tarea asociada al LED_1 */
+}
+
+/**
+ * @brief Función invocada en la interrupción del timer B
+ */
+void FuncTimerB(void* param){
+    xTaskNotifyGive(led2_task_handle);    /* Envía una notificación a la tarea asociada al LED_2 */
+}
+
+/**
+ * @brief Tarea encargada de blinkear el LED_1
+ */
+static void Led1Task(void *pvParameter){
+    while(true){
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);    /* La tarea espera en este punto hasta recibir una notificación */
+        printf("LED_1 Toggle\n");
+        LedToggle(LED_1);
+    }
+}
+
+/**
+ * @brief Tarea encargada de blinkear el LED_2
+ */
+static void Led2Task(void *pvParameter){
+    while(true){
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);    /* La tarea espera en este punto hasta recibir una notificación */
+        printf("LED_2 Toggle\n");
+        LedToggle(LED_2);
+    }
+}
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+    LedsInit();
+    /* Inicialización de timers */
+    timer_config_t timer_led_1 = {
+        .timer = TIMER_A,
+        .period = CONFIG_BLINK_PERIOD_LED_1_US,
+        .func_p = FuncTimerA,
+        .param_p = NULL
+    };
+    TimerInit(&timer_led_1);
+    timer_config_t timer_led_2 = {
+        .timer = TIMER_B,
+        .period = CONFIG_BLINK_PERIOD_LED_2_US,
+        .func_p = FuncTimerB,
+        .param_p = NULL
+    };
+    TimerInit(&timer_led_2);
+    /* Creación de tareas */
+    xTaskCreate(&Led1Task, "LED_1", 512, NULL, 5, &led1_task_handle);
+    xTaskCreate(&Led2Task, "LED_2", 512, NULL, 5, &led2_task_handle);
+    /* Inicialización del conteo de timers */
+    TimerStart(timer_led_1.timer);
+    TimerStart(timer_led_2.timer);
+}
