6f15ced40890050d54abf5d018cf334a2a9b120d
albanopenalva <albano.penalva@uner.edu.ar>
2024-07-23 13:52:16 -0300

    Se modifica driver delay_mcu reemplazando el uso de timers y notificaciones por timers y semaforos binarios para tiempos menores a 100ms y vtaskdelays en los casos de períodos mayores.

--- firmware/drivers/microcontroller/src/delay_mcu.c
+++ firmware/drivers/microcontroller/src/delay_mcu.c
@@ -20,13 +20,14 @@
 #define MSEC				1000	/*!< 1msec = 1000usec */
 #define SEC					1000000	/*!< 1sec = 1000msec */
 #define MIN_US				50	    /*!< minimun delay in usec to use gptimer */
+#define MIN_MS				100	    /*!< minimun delay in msec to use vTaskDelay */
 /*==================[internal data declaration]==============================*/
-TaskHandle_t task_to_notify;
+SemaphoreHandle_t xDelaySemaphore = NULL;
 /*==================[internal functions declaration]=========================*/
 static bool IRAM_ATTR delay_isr(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_data){
 	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
 	gptimer_stop(timer);
-	vTaskNotifyGiveFromISR(task_to_notify, &xHigherPriorityTaskWoken);
+	xSemaphoreGiveFromISR(xDelaySemaphore, &xHigherPriorityTaskWoken);
 	return (xHigherPriorityTaskWoken == pdTRUE);
 }
 /*==================[internal data definition]===============================*/
@@ -37,77 +38,77 @@ static bool IRAM_ATTR delay_isr(gptimer_handle_t timer, const gptimer_alarm_even
 
 /*==================[external functions definition]==========================*/
 void DelaySec(uint16_t sec){
-	task_to_notify = xTaskGetCurrentTaskHandle();
-	gptimer_handle_t delay_timer = NULL;
-    gptimer_config_t delay_timer_config = {
-        .clk_src = GPTIMER_CLK_SRC_DEFAULT,
-        .direction = GPTIMER_COUNT_UP,
-        .resolution_hz = US_RESOLUTION_HZ,
-    };
-    gptimer_new_timer(&delay_timer_config, &delay_timer);
-    gptimer_event_callbacks_t delay_alarm = {
-        .on_alarm = delay_isr,
-    };
-	gptimer_register_event_callbacks(delay_timer, &delay_alarm, NULL);
-	gptimer_enable(delay_timer);
-    gptimer_alarm_config_t alarm_config = {
-        .alarm_count = sec*SEC, 
-    };
-	gptimer_set_alarm_action(delay_timer, &alarm_config);
-	gptimer_start(delay_timer);
-	ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-	gptimer_disable(delay_timer);
-	gptimer_del_timer(delay_timer);
+    vTaskDelay(sec * MSEC / portTICK_PERIOD_MS);
 }
 
 void DelayMs(uint16_t msec){
-	task_to_notify = xTaskGetCurrentTaskHandle();
-	gptimer_handle_t delay_timer = NULL;
-    gptimer_config_t delay_timer_config = {
-        .clk_src = GPTIMER_CLK_SRC_DEFAULT,
-        .direction = GPTIMER_COUNT_UP,
-        .resolution_hz = US_RESOLUTION_HZ,
-    };
-    gptimer_new_timer(&delay_timer_config, &delay_timer);
-    gptimer_event_callbacks_t delay_alarm = {
-        .on_alarm = delay_isr,
-    };
-	gptimer_register_event_callbacks(delay_timer, &delay_alarm, NULL);
-	gptimer_enable(delay_timer);
-    gptimer_alarm_config_t alarm_config = {
-        .alarm_count = msec*MSEC, 
-    };
-	gptimer_set_alarm_action(delay_timer, &alarm_config);
-	gptimer_start(delay_timer);
-	ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-	gptimer_disable(delay_timer);
-	gptimer_del_timer(delay_timer);
+    // If the delay is too short, use the ESP32's internal timer
+    if(msec<=MIN_MS){ 
+        if(xDelaySemaphore == NULL){
+            xDelaySemaphore = xSemaphoreCreateBinary();
+            gptimer_handle_t delay_timer = NULL;
+            gptimer_config_t delay_timer_config = {
+                .clk_src = GPTIMER_CLK_SRC_DEFAULT,
+                .direction = GPTIMER_COUNT_UP,
+                .resolution_hz = US_RESOLUTION_HZ,
+            };
+            gptimer_new_timer(&delay_timer_config, &delay_timer);
+            gptimer_event_callbacks_t delay_alarm = {
+                .on_alarm = delay_isr,
+            };
+            gptimer_register_event_callbacks(delay_timer, &delay_alarm, NULL);
+            gptimer_enable(delay_timer);
+            gptimer_alarm_config_t alarm_config = {
+                .alarm_count = msec*MSEC, 
+            };
+            gptimer_set_alarm_action(delay_timer, &alarm_config);
+            gptimer_start(delay_timer);
+            if(xDelaySemaphore != NULL){
+                xSemaphoreTake(xDelaySemaphore, portMAX_DELAY);
+                xDelaySemaphore = NULL;
+            }
+            gptimer_disable(delay_timer);
+            gptimer_del_timer(delay_timer);
+        }
+    }else{       
+        // If the delay is longer than the minimum delay, use vTaskDelay
+        vTaskDelay(msec / portTICK_PERIOD_MS);
+    }
 }
 
 void DelayUs(uint16_t usec){
     if(usec<=MIN_US){
+        /* If the delay is too short, use the ROM delay function */
         esp_rom_delay_us(usec);
     }else{
-        task_to_notify = xTaskGetCurrentTaskHandle();
-        gptimer_handle_t delay_timer = NULL;
-        gptimer_config_t delay_timer_config = {
-            .clk_src = GPTIMER_CLK_SRC_DEFAULT,
-            .direction = GPTIMER_COUNT_UP,
-            .resolution_hz = US_RESOLUTION_HZ,
-        };
-        gptimer_new_timer(&delay_timer_config, &delay_timer);
-        gptimer_event_callbacks_t delay_alarm = {
-            .on_alarm = delay_isr,
-        };
-        gptimer_register_event_callbacks(delay_timer, &delay_alarm, NULL);
-        gptimer_enable(delay_timer);
-        gptimer_alarm_config_t alarm_config = {
-            .alarm_count = usec, 
-        };
-        gptimer_set_alarm_action(delay_timer, &alarm_config);
-        gptimer_start(delay_timer);
-        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-        gptimer_disable(delay_timer);
-        gptimer_del_timer(delay_timer);
+        /* If the delay is longer than the minimum, use the ESP32's internal timer */
+        if(xDelaySemaphore == NULL){
+	        xDelaySemaphore = xSemaphoreCreateBinary();
+            gptimer_handle_t delay_timer = NULL;
+            gptimer_config_t delay_timer_config = {
+                .clk_src = GPTIMER_CLK_SRC_DEFAULT,
+                .direction = GPTIMER_COUNT_UP,
+                .resolution_hz = US_RESOLUTION_HZ,
+            };
+            gptimer_new_timer(&delay_timer_config, &delay_timer);
+            gptimer_event_callbacks_t delay_alarm = {
+                .on_alarm = delay_isr,
+            };
+            gptimer_register_event_callbacks(delay_timer, &delay_alarm, NULL);
+            gptimer_enable(delay_timer);
+            gptimer_alarm_config_t alarm_config = {
+                .alarm_count = usec, 
+            };
+            gptimer_set_alarm_action(delay_timer, &alarm_config);
+            gptimer_start(delay_timer);
+            if(xDelaySemaphore != NULL){
+                /* Wait for the timer to finish */
+                xSemaphoreTake(xDelaySemaphore, portMAX_DELAY);
+                xDelaySemaphore = NULL;
+            }
+            gptimer_disable(delay_timer);
+            gptimer_del_timer(delay_timer);
+        }
     }
 }
+
